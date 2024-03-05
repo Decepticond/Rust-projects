c983fac31360ec3685b072226aea7eac233d3bfd
Asmodeus <lucillespinkamazon@gmail.com>
2024-03-05 10:12:50 -0500

    Create quick_maths.rs
    4+4 is 8 minus 1 is 7 quick maths

---
+++ quick_maths.rs
@@ -0,0 +1,11 @@
+// I should start rust by roadman where instead of proper naming conventions like in rust by example its just British roadmen 
+
+
+fn main() {
+    println!("Big shaq\n");
+
+    let result: i64 = 4+4;
+    let result_2: i64 = 4+3;
+
+    println!("Four plus four is {result}, minus one is {result_2},... quick maths.");
+}
